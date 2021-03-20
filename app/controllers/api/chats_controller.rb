48cdba20e504adc78c21cf2e1152549300827081
Ahmed Elgokhy <ahmed.elgokhy@Ahmeds-MacBook-Pro-2.local>
2021-03-20 02:27:43 +0200

    Add List and Show endpoints to ChatsController

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -1,5 +1,17 @@
 module Api
   class ChatsController < ApplicationController
+    before_action :set_application, only: %i[index show]
+
+    def index
+      chats = @application.chats
+      render json: chats.as_json(only: :number), status: :ok
+    end
+
+    def show
+      chat = @application.chats.find_by(number: params[:number])
+      render json: chat.as_json(only: :number), status: :ok
+    end
+
     def create
       # chats_count: represent both chats_count in Applications table and chat_number in Chats table
       # It will increment if exists or intialize with 1
@@ -7,6 +19,12 @@ module Api
       HandleChatCreationWorker.perform_async(chats_count, params[:application_token])
       
       render json: { chat_number: chats_count }, status: :ok
+    end
+
+    private
+
+    def set_application
+      @application = Application.find_by token: params[:application_token]
     end
   end
 end
