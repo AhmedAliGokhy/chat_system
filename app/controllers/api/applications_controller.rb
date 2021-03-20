b4bf4672b2e66bfb46d21163cb55a6df4b370f78
Ahmed Elgokhy <ahmed.elgokhy@Ahmeds-MacBook-Pro-2.local>
2021-03-20 02:14:28 +0200

    Add List, Show and Update endpoints to ApplictionsController

--- app/controllers/api/applications_controller.rb
+++ app/controllers/api/applications_controller.rb
@@ -1,12 +1,32 @@
 module Api
   class ApplicationsController < ApplicationController
+    def index
+      applications = Application.all
+      render json: applications.as_json(only: [:name, :token]), status: :ok
+    end
+
+    def show
+      application = Application.find_by token: params[:token]
+      render json: application.as_json(only: [:name, :token]), status: :ok
+    end
+
     def create
       application = Application.new(application_params)
 
       if application.save
         REDIS_CLIENT.set(application.token, 0)
         
-        render json: { token: application.token }, status: :ok
+        render json: application.as_json(only: [:name, :token]), status: :ok
+      else
+        render json: application.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      application = Application.find_by token: params[:token]
+
+      if application.update(application_params)
+        render json: application.as_json(only: [:name, :token]), status: :ok
       else
         render json: application.errors, status: :unprocessable_entity
       end
