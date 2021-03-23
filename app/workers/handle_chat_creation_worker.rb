cd9a7c0734f2c6144856e7f20435f56cd57f8d1a
Ahmed Elgokhy <ahmed.elgokhy@Ahmeds-MacBook-Pro-2.local>
2021-03-24 01:04:51 +0200

    handle chat insertion

--- app/workers/handle_chat_creation_worker.rb
+++ app/workers/handle_chat_creation_worker.rb
@@ -6,7 +6,7 @@ class HandleChatCreationWorker
     application.update(chats_count: chats_count) if application.chats_count < chats_count
 
     # Insert chat record
-    application.chats.create(number: chats_count)
+    application.chats.where(number: chats_count).first_or_create
 
     # Insert new key in redis to store messages_count
     REDIS_CLIENT.set("#{application.token}_#{chats_count}", 0)
